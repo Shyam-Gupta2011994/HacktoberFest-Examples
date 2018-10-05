487f6f12ef77bc35d4253f09b98dc5c140b0ad5b
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-10-05 13:58:10 +0530

    helloworld in rust

---
+++ helloworld.rs
@@ -0,0 +1,16 @@
+// This is a comment, and will be ignored by the compiler
+
+// This is the main function
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
+
+// Running the code:
+// A binary can be generated using the Rust compiler: rustc
+// rustc helloworld.rs
+// rustc will produce a helloworld binary that can be executed
+// $ ./hellowrold
+// Hello World!
